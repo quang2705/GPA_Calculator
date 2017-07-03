894465f53a69ba13c88ca366df4cd03313dfe624
Quang Nguyen <quang2705@users.noreply.github.com>
2017-07-03 10:21:34 +0800

    Add files via upload

--- testProject.js
+++ testProject.js
@@ -1,7 +1,7 @@
-
-function createTest(i=0){
-	var number = document.getElementsByClassName("theId")[i].value;
-	var result = document.getElementsByClassName("result")[i];
+function createTest(n){
+	n = parseInt(n);
+	var number = document.getElementsByClassName("theId")[n].value;
+	var result = document.getElementsByClassName("result")[n];
 	while (result.firstChild){
 		result.removeChild(result.firstChild);
 	}
@@ -11,7 +11,7 @@ function createTest(i=0){
 		{
 			var input = document.createElement("INPUT");
 			input.setAttribute("type","text");
-			input.setAttribute("oninput","test_score(i)");
+			input.setAttribute("oninput","test_score("+n+")");
 			result.appendChild(input);
 			result.appendChild(document.createElement("br"));
 		}
@@ -32,14 +32,16 @@ function testCreate(){
 		var per = document.getElementsByClassName("percentage")[i];
 		var miniTest = document.getElementsByClassName("theId")[i];
 		var n = i;
-		miniTest.oninput = function(){createTest(n)};
-		per.oninput = function(){Compute_percentage(n)};
+		miniTest.setAttribute("oninput","createTest("+n+")");
+		per.setAttribute("oninput","Compute_percentage("+n+")");
 	}
 }
 
-function Compute_percentage(i=0){
-	var percentage = document.getElementsByClassName("percentage")[i].value;
-	var array = test_score();
+function Compute_percentage(n){
+	n = parseInt(n);
+	var percentage = document.getElementsByClassName("percentage")[n].value;
+	var array = test_score(n);
+	var grade_and_percentage = [];
 	var total = 0;
 	var denominator = 0;
 	for (var i = 0; i < array.length; i++){
@@ -50,13 +52,17 @@ function Compute_percentage(i=0){
 		}
 	}
 	var final_grade = total/denominator;
-	document.getElementById("current-grade").innerHTML = final_grade;
+	grade_and_percentage.push(final_grade);
+	grade_and_percentage.push(percentage);
+	document.getElementsByClassName("current-grade")[n].innerHTML = final_grade;
+	return grade_and_percentage;
 }
 
-function test_score(i=0){
+function test_score(n){
+	n = parseInt(n);
 	var test_array = [];
-	var number = document.getElementsByClassName("theId")[i].value;
-	var result = document.getElementsByClassName("result")[i];
+	var number = document.getElementsByClassName("theId")[n].value;
+	var result = document.getElementsByClassName("result")[n];
 	for (var i = 0 ; i < number; i++)
 	{
 		var score = result.children[i*2].value;
@@ -65,3 +71,20 @@ function test_score(i=0){
 	return test_array;
 }
 
+function GPA_grade(){
+	var val = document.getElementById("total").value;
+	var percent_total = 0;
+	var grade_percent =[];
+	var gpa= 0;
+	for (var i =0; i< val ; i ++){
+		var array = Compute_percentage(i);
+		console.log(array);
+		percent_total += parseInt(array[1]);
+	}
+	console.log(percent_total);
+	for (var i = 0 ; i <val; i++){
+		var grade_percent = Compute_percentage(i);
+		gpa += grade_percent[0]*grade_percent[1]/percent_total;
+	}	
+	document.getElementById("final_grade").innerHTML = gpa;
+}
