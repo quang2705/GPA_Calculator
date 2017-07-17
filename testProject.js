f87a1f1099585746de58c8d820a8f9cfdaa6cc5d
Quang Nguyen <quang2705@users.noreply.github.com>
2017-07-17 15:02:21 +0800

    Add files via upload

--- testProject.js
+++ testProject.js
@@ -1,53 +1,55 @@
 function createTest(n){
 	n = parseInt(n);
-	var number = document.getElementsByClassName("theId")[n].value;
-	var result = document.getElementsByClassName("result")[n];
-	while (result.firstChild){
-		result.removeChild(result.firstChild);
+	var number_miniTest = document.getElementsByClassName("smallTest")[n].value;
+	var list_miniTest = document.getElementsByClassName("list_miniTest")[n];
+	while (list_miniTest.firstChild){
+		list_miniTest.removeChild(list_miniTest.firstChild);
 	}
-	if (number <= 20)
-	{	
-		for (var i = 0 ; i < number; i++)
-		{
+	if (number_miniTest <= 20){	
+		for (var i = 0 ; i < number_miniTest; i++){
 			var input = document.createElement("INPUT");
 			input.setAttribute("type","text");
 			input.setAttribute("oninput","test_score("+n+")");
-			result.appendChild(input);
-			result.appendChild(document.createElement("br"));
+			list_miniTest.appendChild(input);
+			list_miniTest.appendChild(document.createElement("br"));
 		}
 	}
 }
 
-function testCreate(){
-	var val = document.getElementById("total").value;
-	var con = document.getElementById("content");
-	var percent_each_test = 100/val;
+function CreateBigTest(){
 	var per = document.getElementsByClassName("percentage")[0];
+	var bt = document.getElementById("bigTest").value;
+	var con = document.getElementById("content");
+	var percent_each_test = 100/bt;
 	per.value = percent_each_test;
+	
 	while (con.firstChild){
 		con.removeChild(con.firstChild);
 	}
-	for (var i = 1; i <val ; i++)
-	{	
-		var original = document.getElementsByClassName("default")[0];
-		var clone = original.cloneNode(true); 
-		con.appendChild(clone);
-		var per = document.getElementsByClassName("percentage")[i];
-		var miniTest = document.getElementsByClassName("theId")[i];
-		var n = i;
-		miniTest.setAttribute("oninput","createTest("+n+")");
-		per.setAttribute("oninput","Compute_percentage("+n+")");
-		per.setAttribute("value", percent_each_test);
+	
+	if (bt <= 15){
+		for (var i = 1; i <bt ; i++){	
+			var original = document.getElementsByClassName("default")[0];
+			var clone = original.cloneNode(true); 
+			con.appendChild(clone);
+			var per = document.getElementsByClassName("percentage")[i];
+			var miniTest = document.getElementsByClassName("smallTest")[i];
+			var n = i;
+			miniTest.setAttribute("oninput","createTest("+n+")");
+			per.setAttribute("oninput","Grade_and_percentage("+n+")");
+			per.setAttribute("value", percent_each_test);
+		}
 	}
 }
 
-function Compute_percentage(n){
+function Grade_and_percentage(n){
 	n = parseInt(n);
 	var percentage = document.getElementsByClassName("percentage")[n].value;
-	var array = test_score(n);
 	var grade_and_percentage = [];
-	var total = 0;
+	var array = test_score(n);
 	var denominator = 0;
+	var total = 0;
+
 	for (var i = 0; i < array.length; i++){
 		if (array[i] != "")
 		{
@@ -65,11 +67,11 @@ function Compute_percentage(n){
 function test_score(n){
 	n = parseInt(n);
 	var test_array = [];
-	var number = document.getElementsByClassName("theId")[n].value;
-	var result = document.getElementsByClassName("result")[n];
-	for (var i = 0 ; i < number; i++)
-	{
-		var score = result.children[i*2];
+	var number_miniTest = document.getElementsByClassName("smallTest")[n].value;
+	var list_miniTest = document.getElementsByClassName("list_miniTest")[n];
+	
+	for (var i = 0 ; i < number_miniTest; i++){
+		var score = list_miniTest.children[i*2];
 		if (score.style.color == "red"){
 			test_array.push("");
 		}
@@ -80,43 +82,42 @@ function test_score(n){
 }
 
 function GPA_grade(){
-	var val = document.getElementById("total").value;
+	var bt = document.getElementById("bigTest").value;
 	var percent_total = 0;
 	var grade_percent =[];
 	var gpa= 0;
-	for (var i =0; i< val ; i ++){
-		var array = Compute_percentage(i);
-		console.log(array);
+	for (var i =0; i< bt ; i ++){
+		var array = Grade_and_percentage(i);
 		percent_total += parseInt(array[1]);
 	}
-	console.log(percent_total);
-	for (var i = 0 ; i <val; i++){
-		var grade_percent = Compute_percentage(i);
+
+	for (var i = 0 ; i <bt; i++){
+		var grade_percent = Grade_and_percentage(i);
 		gpa += grade_percent[0]*grade_percent[1]/percent_total;
 	}	
 	document.getElementById("final_grade").innerHTML = gpa;
 }
 
 function Estimate(){
+	var bt = document.getElementById("bigTest").value;
+	var goal = document.getElementById("goal").value;
 	var current_test_score = 0
 	var estimate_percentage = 0
-	var val = document.getElementById("total").value;
-	for (var i = 0; i < val; i++){
+	for (var i = 0; i < bt; i++){
+		var per = document.getElementsByClassName("percentage")[i].value/100;
 		var unknow = 0
 		var test_array = test_score(i);
-		var per = document.getElementsByClassName("percentage")[i].value/100;
 		var per_each_test = per/test_array.length;
 		for (var n = 0; n < test_array.length; n++){
 			if (test_array[n] ==""){
 				unknow += 1;
 			}
-			else{
+			else
 				current_test_score += parseInt(test_array[n])*per_each_test;
-			}
 		}
 		estimate_percentage += unknow*per_each_test;
 	}
-	var goal = document.getElementById("goal").value;
+
 	if (estimate_percentage != 0){
 		var estimate_score = ((parseInt(goal)-current_test_score))/estimate_percentage;
 		if (estimate_score >100){
@@ -129,13 +130,14 @@ function Estimate(){
 		document.getElementById("warning").innerHTML = "YOU DONT HAVE ANY TEST LEFT TO ESTIMATE";
 		return;
 	}
-	for (var i = 0; i < val ; i++ ){
-		var result = document.getElementsByClassName("result")[i];
-		var number = document.getElementsByClassName("theId")[i].value;
-		for (var n = 0 ; n < number; n++){
-			var score = result.children[n*2];
+
+	for (var i = 0; i < bt ; i++ ){
+		var list_miniTest = document.getElementsByClassName("list_miniTest")[i];
+		var number_miniTest = document.getElementsByClassName("smallTest")[i].value;
+		for (var n = 0 ; n < number_miniTest; n++){
+			var score = list_miniTest.children[n*2];
 			if (score.value == "" || score.style.color == "red"){
-				result.children[n*2].value = estimate_score;
+				list_miniTest.children[n*2].value = estimate_score;
 				score.style.color = "red";
 			}
 		}
