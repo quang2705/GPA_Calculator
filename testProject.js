5ddb5c37d5adb3b87eb9363b603f9425eab10764
Quang Nguyen <quang2705@users.noreply.github.com>
2017-07-17 12:17:51 +0800

    Add files via upload

--- testProject.js
+++ testProject.js
@@ -21,6 +21,9 @@ function createTest(n){
 function testCreate(){
 	var val = document.getElementById("total").value;
 	var con = document.getElementById("content");
+	var percent_each_test = 100/val;
+	var per = document.getElementsByClassName("percentage")[0];
+	per.value = percent_each_test;
 	while (con.firstChild){
 		con.removeChild(con.firstChild);
 	}
@@ -34,6 +37,7 @@ function testCreate(){
 		var n = i;
 		miniTest.setAttribute("oninput","createTest("+n+")");
 		per.setAttribute("oninput","Compute_percentage("+n+")");
+		per.setAttribute("value", percent_each_test);
 	}
 }
 
@@ -48,7 +52,7 @@ function Compute_percentage(n){
 		if (array[i] != "")
 		{
 			denominator +=1;
-			total += parseInt(array[i]);
+			total += parseFloat(array[i]);
 		}
 	}
 	var final_grade = total/denominator;
@@ -65,8 +69,12 @@ function test_score(n){
 	var result = document.getElementsByClassName("result")[n];
 	for (var i = 0 ; i < number; i++)
 	{
-		var score = result.children[i*2].value;
-		test_array.push(score);
+		var score = result.children[i*2];
+		if (score.style.color == "red"){
+			test_array.push("");
+		}
+		else
+			test_array.push(score.value);
 	}
 	return test_array;
 }
@@ -87,4 +95,43 @@ function GPA_grade(){
 		gpa += grade_percent[0]*grade_percent[1]/percent_total;
 	}	
 	document.getElementById("final_grade").innerHTML = gpa;
+}
+
+function Estimate(){
+	var current_test_score = 0
+	var estimate_percentage = 0
+	var val = document.getElementById("total").value;
+	for (var i = 0; i < val; i++){
+		var unknow = 0
+		var test_array = test_score(i);
+		var per = document.getElementsByClassName("percentage")[i].value/100;
+		var per_each_test = per/test_array.length;
+		for (var n = 0; n < test_array.length; n++){
+			if (test_array[n] ==""){
+				unknow += 1;
+			}
+			else{
+				current_test_score += parseInt(test_array[n])*per_each_test;
+			}
+		}
+		estimate_percentage += unknow*per_each_test;
+	}
+	var goal = document.getElementById("goal").value;
+	if (estimate_percentage != 0){
+		var estimate_score = ((parseInt(goal)-current_test_score))/estimate_percentage;
+		estimate_score = estimate_score.toString();
+	}
+	else
+		console.log("ERROR");
+	for (var i = 0; i < val ; i++ ){
+		var result = document.getElementsByClassName("result")[i];
+		var number = document.getElementsByClassName("theId")[i].value;
+		for (var n = 0 ; n < number; n++){
+			var score = result.children[n*2];
+			if (score.value == "" || score.style.color == "red"){
+				result.children[n*2].value = estimate_score;
+				score.style.color = "red";
+			}
+		}
+	}
 }
